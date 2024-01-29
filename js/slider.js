62b3a949ac58e8793a2bb05c84b29d958fb49077
gon_14 <poniemangon@gmail.com>
2024-01-29 16:33:20 -0300

    finalizado sin carousel

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,31 @@
 $('.carousel').slick({
-    infinite: true,
-    // arrows: true,
-    // nextArrow: false,
-    // prevArrow: false,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [{
+      breakpoint: 1190,
+      settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+      }
+  },
+  {
+      breakpoint: 600,
+      settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+
+      }
+  },
+  {
+      breakpoint: 480,
+      settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+
+      }
+  }]
+});
+
