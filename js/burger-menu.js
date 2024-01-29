b5f2af17f33d232516f9f819a10d7e09e507aeb9
gon_14 <poniemangon@gmail.com>
2024-01-29 16:32:31 -0300

    first commit

---
+++ js/burger-menu.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $(".fa-bars").click(function() {
+    $(".navbar-mobile ul").slideToggle();
+    });
+  });
---
+++ js/slider.js
@@ -0,0 +1,8 @@
+$('.carousel').slick({
+    infinite: true,
+    // arrows: true,
+    // nextArrow: false,
+    // prevArrow: false,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
